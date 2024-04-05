deab9015405d5b1f09e2dcc115ae244ae3687fa2
Nguyen Minh Kiet <nguyenminhkiet642002@gmail.com>
2024-04-06 01:23:59 +0700

    update source

--- layout/xuly/admin-xuly/getAdminSignin.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if(isset($_POST['name'])){
-        echo $_POST['name'];
-    } else if(isset($_POST['username'])){
-        echo $_POST['username'];
-    } else{
-        echo "Null admin";
-    }
-?>
